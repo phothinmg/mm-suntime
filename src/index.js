fd30b91138f8cb9404dbd1cde9edb8d2231ced5e
phothinmg <phothinmg@gmail.com>
2024-04-01 23:25:27 +0630

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -22,10 +22,26 @@ function daytime() {
   const loct = dtt.getTime();
   const loctzof = dtt.getTimezoneOffset() * 60 * 1000;
   const utcn = loct + loctzof;
+  const localdate = dtt.toDateString();
+  setInterval(function () {
+    var dt_3 = new Date();
+    var dat_3 = dt_3.toLocaleTimeString();
+    document.getElementById("usertime").innerHTML =  `Current Time : <span>${dat_3}</span>`;
+  }, 1000);
+  // -----------------------------------------------------------------//
   const mytime = utcn + MYANMAR_TIMEZONE_OFFSET_MS;
-  const mmm = new Date(mytime).toString().split(" ");
-  const mmdate = mmm.slice(1, 4).join("-");
-  const mmtime = mmm[4];
+  const mmm = new Date(mytime).toDateString();
+  setInterval(function () {
+    const MYANMAR_TIMEZONE_OFFSET_MS_2 = 390 * 60 * 1000;
+    const dtt_2 = new Date();
+    const loct_2 = dtt_2.getTime();
+    const loctzof_2 = dtt_2.getTimezoneOffset() * 60 * 1000;
+    const utcn_2 = loct_2 + loctzof_2;
+    const myytime = utcn_2 + MYANMAR_TIMEZONE_OFFSET_MS_2;
+    const dt = new Date(myytime);
+    const dat = dt.toLocaleTimeString();
+    document.getElementById("mmclock").innerHTML = `Current Time : <span>${dat}</span>`;
+  }, 1000);
   //-----------------------------------------------------
   const view = document.getElementById("v");
   view.innerHTML = "";
@@ -57,15 +73,18 @@ function daytime() {
   <span class='rg-dek' style="font-weight: bold;">User's Location</span>
   <br>
   <span class='rg-dek' style="font-size: small;">Timezone : ${usrtz}</span>
+  <span class='rg-dek' style="font-size: small;">Current Date : ${localdate}</span>
+  <span class='rg-dek' style="font-size: small;" id="usertime"></span>
   <span class='rg-dek' style="font-size: small;" id="sr"></span>
   <span class='rg-dek' style="font-size: small;" id="ss"></span>
   <span class='rg-dek' style="font-size: small;" id="dayt"></span>
   <br>
   <hr>
   <br>
-  <span class='rg-dek' style="font-weight: bold;">${mmdate}</span>
+  <span class='rg-dek' style="font-weight: bold;">Myanmar GMT+0630</span>
   <br>
-  <span class='rg-dek' style="font-size: small;">${mmtime}</span>
+  <span class='rg-dek' style="font-size: small;">Current Date : ${mmm}</span>
+  <span class='rg-dek' style="font-size: small;" id="mmclock"></span>
   <br>
   <hr>
   `;
